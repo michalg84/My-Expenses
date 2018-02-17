6e0e99d5a9f25c5b392eef2337cea148970dc1b9
michalg84 <michalgalka84@gmail.com>
2018-02-17 22:16:58 +0100

    Logger attached to message service

--- src/main/java/pl/sda/service/MessageServiceImpl.java
+++ src/main/java/pl/sda/service/MessageServiceImpl.java
@@ -1,5 +1,6 @@
 package pl.sda.service;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import pl.sda.dto.MessageDto;
@@ -12,31 +13,37 @@ import java.util.List;
  * Created by Michał Gałka on 2017-05-22.
  */
 @Service
-public class MessageServiceImpl implements MessageService{
+public class MessageServiceImpl implements MessageService {
+    public static final String NOTIFY_MSG_SESSION_KEY = "notificationMessages";
+    private static final Logger logger_ = Logger.getLogger(MessageServiceImpl.class);
     @Autowired
     private HttpSession httpSession;
-    public static final String NOTIFY_MSG_SESSION_KEY = "notificationMessages";
 
     public void addInfoMessage(String msg) {
         addNotificationMessage(MessageDto.MessageDtoType.INFO, msg);
+        logger_.info(msg);
     }
 
     public void addSuccessMessage(String msg) {
         addNotificationMessage(MessageDto.MessageDtoType.SUCCESS, msg);
+        logger_.info(msg);
     }
 
     public void addWarnMessage(String msg) {
         addNotificationMessage(MessageDto.MessageDtoType.WARN, msg);
+        logger_.warn(msg);
     }
 
     public void addErrorMessage(String msg) {
         addNotificationMessage(MessageDto.MessageDtoType.ERROR, msg);
+        logger_.error(msg);
     }
+
 
     public void addNotificationMessage(MessageDto.MessageDtoType type, String msg) {
         List<MessageDto> notifyMessages = (List<MessageDto>) httpSession.getAttribute(NOTIFY_MSG_SESSION_KEY);
         if (notifyMessages == null) {
-            notifyMessages = new ArrayList<MessageDto>();
+            notifyMessages = new ArrayList<>();
         }
         notifyMessages.add(new MessageDto(type, msg));
         httpSession.setAttribute(NOTIFY_MSG_SESSION_KEY, notifyMessages);
